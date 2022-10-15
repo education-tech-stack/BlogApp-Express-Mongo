0d00aa4cb533cd39fbc5d6899e459f3ecb840701
Sameer Chauhan <sameer@extensioncrm.com>
2022-10-16 02:39:57 +0530

    created seperate start for heruko

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    if(req.session.userId) return res.render('create')
+    if(req.session.userId) return res.render('create',{createPost: true})
     else res.redirect('/auth/login')
 }
--- index.js
+++ index.js
@@ -40,7 +40,11 @@ app.use("*", (req, res, next) => {
     next()
 })
 
-app.listen(4000, () => {
+let port = process.env.PORT
+if(port == null || port == ""){
+    port = 4000;
+}
+app.listen(port, () => {
     console.log('App listening on port 4000')
 })
 
